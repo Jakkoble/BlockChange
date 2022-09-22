be8dc9032c4be120c1fc1959718e18506603c4f5
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-09-22 12:22:19 +0200

    Added DefaultBlock Item Enum

---
+++ src/main/kotlin/de/jakkoble/ressources/DefaultBlocks.kt
@@ -0,0 +1,11 @@
+package de.jakkoble.ressources
+
+import org.bukkit.Material
+
+enum class DefaultBlocks(val material: Material) {
+   STONE(Material.STONE),
+   COAL(Material.COAL_ORE),
+   IRON(Material.IRON_ORE),
+   DIAMOND(Material.DIAMOND_ORE),
+   DIRT(Material.DIRT)
+}
