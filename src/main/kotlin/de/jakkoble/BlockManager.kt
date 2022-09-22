776964d8fe8d308ebb1d92dc397608693d3a55e2
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-09-22 12:22:31 +0200

    Uploaded Latest

--- src/main/kotlin/de/jakkoble/BlockManager.kt
+++ src/main/kotlin/de/jakkoble/BlockManager.kt
@@ -3,23 +3,22 @@ package de.jakkoble
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
-import de.jakkoble.enums.Wood
-import org.bukkit.Material
-import org.bukkit.Registry.MATERIAL
-import org.bukkit.entity.Player
+import de.jakkoble.ressources.Ore
+import de.jakkoble.ressources.Stone
+import de.jakkoble.ressources.Wood
+import de.jakkoble.ressources.getMaterials
 import java.io.File
-import kotlin.random.Random
 
 val userData = mutableListOf<UserData>()
 
 class BlockManager {
    init {
-      /*Main.INSTANCE.saveConfig()
-      val configFile = File("plugins/DuelUltimate/userData.json")
+      Main.INSTANCE.saveConfig()
+      val configFile = File("plugins/FaisterSMP/userData.json")
       if (!configFile.exists()) {
          configFile.createNewFile()
          configFile.writeText(GsonBuilder().setPrettyPrinting().create().toJson(userData))
-      }*/
+      }
    }
    fun load() {
       val configFile = File("plugins/FaisterSMP/userData.json")
@@ -27,13 +26,14 @@ class BlockManager {
       if (data != null) userData.addAll(data)
    }
    fun generateRandomBlocks() {
-      val bannedMaterial = listOf(Material.AIR, Material.COMMAND_BLOCK, Material.COMMAND_BLOCK_MINECART, Material.REPEATING_COMMAND_BLOCK, Material.CHAIN_COMMAND_BLOCK)
-      val materials = Material.values().filter { it.isBlock && !it.isLegacy && !bannedMaterial.contains(it)}
-      println("Materials Size: ${materials}")
-      val wood = Wood.values()[Random.nextInt(0, Wood.values().size - 1)]
+      val wood = Wood.values()[(0 until Wood.values().size).random()]
       println("Wood: $wood")
-      for (i in 1..randomBlocks)
-         println("Run $i")
+      println(wood.getMaterials())
+      val stone = Stone.values()[(0 until Stone.values().size).random()]
+      println("Stone: $stone")
+      println(stone.getMaterials())
+      val ore = Ore.values()[(0 until Ore.values().size).random()]
+      println("Ore: $ore")
+      println(ore.getMaterials())
    }
 }
-fun Player.newBlocks(): Boolean = Main.INSTANCE.config.getBoolean(ConfigPath.RANDOM_BLOCKS.path)
