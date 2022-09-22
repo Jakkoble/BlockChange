8238ae02bfe7d9c51d763b787e4f64d9698bb0d5
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-09-22 12:21:42 +0200

    Added Wood Item Enum

---
+++ src/main/kotlin/de/jakkoble/ressources/Wood.kt
@@ -0,0 +1,17 @@
+package de.jakkoble.ressources
+
+import org.bukkit.Material
+
+enum class Wood(val material: Material) {
+   ACACIA(Material.ACACIA_WOOD),
+   BIRCH(Material.BIRCH_WOOD),
+   DARK_OAK(Material.DARK_OAK_WOOD),
+   JUNGLE(Material.JUNGLE_WOOD),
+   OAK(Material.OAK_WOOD),
+   SPRUCE(Material.SPRUCE_WOOD)
+}
+fun Wood.getMaterials(): List<Material> = Material.values().filter {
+   it.isSolid && it.name.startsWith(name)
+      || it.name.startsWith("PETRIFIED_${name}")
+      || it.name.startsWith("STRIPPED_${name}")
+      || it.name.startsWith("POTTED_${name}") }
