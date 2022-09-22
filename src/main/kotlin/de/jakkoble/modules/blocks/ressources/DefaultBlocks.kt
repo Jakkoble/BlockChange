0eed3b575c2bf86e8c1e484f9472a79ec1abeeb6
Jakkoble <business@jakkoble.de>
2022-09-22 21:38:08 +0200

    Added DefaultBlocks

---
+++ src/main/kotlin/de/jakkoble/modules/blocks/ressources/DefaultBlocks.kt
@@ -0,0 +1,17 @@
+package de.jakkoble.modules.blocks.ressources
+
+import org.bukkit.Material
+
+enum class DefaultBlocks(val material: Material) {
+   STONE(Material.STONE),
+   COAL(Material.COAL_ORE),
+   IRON(Material.IRON_ORE),
+   DIAMOND(Material.DIAMOND_ORE),
+   DIRT(Material.DIRT),
+   CHEST(Material.CHEST),
+   CRAFTING_TABLE(Material.CRAFTING_TABLE),
+   FURNACE(Material.FURNACE)
+}
+fun DefaultBlocks.getMaterials(): List<Material> = Material.values().filter {
+   it.isSolid && !it.isLegacy && it.name.contains(name)
+}
