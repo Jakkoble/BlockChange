0224289ce4f6dd2ea626c5eb2d67f2fded71de9f
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-09-22 12:22:03 +0200

    Added Ore Item Enum

---
+++ src/main/kotlin/de/jakkoble/ressources/Ore.kt
@@ -0,0 +1,15 @@
+package de.jakkoble.ressources
+
+import org.bukkit.Material
+
+enum class Ore(val material: Material) {
+   COPPER(Material.COPPER_ORE),
+   EMERALD(Material.EMERALD_BLOCK),
+   GOLD(Material.GOLD_BLOCK),
+   LAPIS(Material.LAPIS_ORE),
+   QUARTZ(Material.NETHER_QUARTZ_ORE),
+   REDSTONE(Material.REDSTONE_ORE)
+}
+fun Ore.getMaterials(): List<Material> = Material.values().filter {
+   it.isSolid && it.name.contains(name)
+}
