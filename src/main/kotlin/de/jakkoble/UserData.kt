9d33911d62f70d2504c2598ba640bd62fd0e7097
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-09-22 12:21:50 +0200

    Added UserData Class

--- src/main/kotlin/de/jakkoble/UserData.kt
+++ src/main/kotlin/de/jakkoble/UserData.kt
@@ -1,6 +1,5 @@
 package de.jakkoble
 
-import de.jakkoble.enums.Wood
 import org.bukkit.Material
 
-data class UserData(val name: String, val uuid: String, val newBlocks: Boolean, val wood: Wood, val blocks: MutableList<Material>)
+data class UserData(val name: String, val uuid: String, val newBlocks: Boolean, val blocks: MutableList<Material>)
