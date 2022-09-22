2b67a635b9d53b2e6e11a95e8d8817cd29a625e0
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-09-22 12:21:33 +0200

    Added Stone Item Enum

---
+++ src/main/kotlin/de/jakkoble/ressources/Stone.kt
@@ -0,0 +1,20 @@
+package de.jakkoble.ressources
+
+import org.bukkit.Material
+
+enum class Stone(val icon: Material) {
+   ANDESITE(Material.ANDESITE),
+   BLACKSTONE(Material.BLACKSTONE),
+   CALCITE(Material.CALCITE),
+   DEEPSLATE(Material.DEEPSLATE),
+   SAND(Material.SAND),
+   TUFF(Material.TUFF),
+   GRANITE(Material.GRANITE),
+   END_STONE(Material.END_STONE),
+   PRISMARINE(Material.PRISMARINE),
+   PURPUR(Material.PURPUR_BLOCK),
+   NETHERRACK(Material.NETHERRACK)
+}
+fun Stone.getMaterials(): List<Material> = Material.values().filter {
+   it.isSolid && !it.isLegacy && it.name.contains(name) && !it.name.contains("ORE")
+}
