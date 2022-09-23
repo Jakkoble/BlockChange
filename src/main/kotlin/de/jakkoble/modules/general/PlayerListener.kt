e9d1d7344aeea87deefb1c9b299cdeaf452effea
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-09-23 08:39:50 +0200

    Added Listener

--- src/main/kotlin/de/jakkoble/modules/general/PlayerListener.kt
+++ src/main/kotlin/de/jakkoble/modules/general/PlayerListener.kt
@@ -1,16 +1,17 @@
-package de.jakkoble
+package de.jakkoble.modules.general
 
+import de.jakkoble.modules.blocks.generateBlocks
 import de.jakkoble.modules.blocks.isAllowedToGetBlock
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockDropItemEvent
-import org.bukkit.event.entity.ItemSpawnEvent
+import org.bukkit.event.player.PlayerJoinEvent
 
 class PlayerListener : Listener {
    @EventHandler
    fun onBreakBlock(event: BlockDropItemEvent) {
-      println(event.blockState.type)
       if (!event.player.isAllowedToGetBlock(event.blockState.type)) event.isCancelled = true
    }
+   @EventHandler
+   fun onPlayerJoin(event: PlayerJoinEvent) = event.player.generateBlocks()
 }
