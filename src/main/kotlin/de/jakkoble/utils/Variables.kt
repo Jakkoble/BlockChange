735a0f89f73f823361129c9bdd4b373d013800eb
Jakkoble <business@jakkoble.de>
2022-09-24 17:07:53 +0200

    Added savePrefix (Component)

--- src/main/kotlin/de/jakkoble/utils/Variables.kt
+++ src/main/kotlin/de/jakkoble/utils/Variables.kt
@@ -1,9 +1,15 @@
 package de.jakkoble.utils
 
-import de.jakkoble.modules.settings.Intervall
+import de.jakkoble.modules.settings.Interval
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.ChatColor
 
 val prefix = "${ChatColor.GOLD}FaisterSMP ${ChatColor.WHITE}•${ChatColor.GRAY}"
+val savePrefix = Component.text()
+   .append(Component.text("FaisterSMP ").color(NamedTextColor.GOLD))
+   .append(Component.text("•").color(NamedTextColor.WHITE))
+   .build()
 var randomBlocks: Long = 0
 var latestRole: Long = 0
-var blockIntervall: Intervall = Intervall.THREE_DAYS
+var blockInterval: Interval = Interval.THREE_DAYS
