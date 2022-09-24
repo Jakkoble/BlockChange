c7d03a6aebca828e16cf9d748431b9c6923ebd2c
Jakkoble <business@jakkoble.de>
2022-09-24 17:07:11 +0200

    Added MaterialCategory Enum

---
+++ src/main/kotlin/de/jakkoble/modules/blocks/resources/MaterialCategory.kt
@@ -0,0 +1,24 @@
+package de.jakkoble.modules.blocks.resources
+
+import de.jakkoble.modules.data.PlayerData
+import org.bukkit.Material
+
+enum class MaterialCategory() {
+   COLORED_BLOCKS,
+   DEFAULT_BLOCKS,
+   PERSONAL_BLOCKS
+}
+fun String.getMaterialCategoryByName(): MaterialCategory? = MaterialCategory.values().firstOrNull { it.name == this }
+fun getMaterialListByCategory(materialCategory: MaterialCategory, data: PlayerData): List<Material?> = when (materialCategory) {
+   MaterialCategory.COLORED_BLOCKS -> data.color.getMaterials()
+   MaterialCategory.DEFAULT_BLOCKS -> DefaultBlocks.values().flatMap { it.getMaterials() }
+   MaterialCategory.PERSONAL_BLOCKS -> getPersonalBlocks(data)
+}
+private fun getPersonalBlocks(data: PlayerData): List<Material> {
+   val materials = mutableListOf<Material>()
+   materials.addAll(data.ore.getMaterials())
+   materials.addAll(data.otherBlocks.flatMap { it.getMaterials() })
+   materials.addAll(data.stone.getMaterials())
+   materials.addAll(data.wood.getMaterials())
+   return materials
+}
