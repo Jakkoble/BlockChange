12ca67f5e95a226ffc579e5221e8224bb483ed9f
Jakkoble <business@jakkoble.de>
2022-09-24 19:03:42 +0200

    Code Cleanup

--- src/main/kotlin/de/jakkoble/modules/settings/SettingsManager.kt
+++ src/main/kotlin/de/jakkoble/modules/settings/SettingsManager.kt
@@ -1,18 +1,23 @@
 package de.jakkoble.modules.settings
 
-import de.jakkoble.utils.*
+import de.jakkoble.utils.Item
+import de.jakkoble.utils.createItem
+import de.jakkoble.utils.fillBorder
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.inventory.Inventory
 
 class SettingsManager {
    fun getInventory(): Inventory {
       val inventory = Bukkit.createInventory(null, 5*9, Component.text("Einstellungen"))
       inventory.fillBorder()
+      inventory.setItem(36, createItem(
+         material = Material.DARK_OAK_DOOR,
+         item = Item.BLOCK_RETURN,
+         name = "Zurück",
+         lore = listOf("Kehre zum Hauptmenü zurück")
+      ))
       return inventory
-   }
-   fun setRolePeriod(interval: Interval) {
-      blockInterval = interval
-      Config().set(ConfigPath.BLOCK_INTERVALL, interval.name)
    }
 }
