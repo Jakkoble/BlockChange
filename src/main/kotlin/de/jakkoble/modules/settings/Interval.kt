51f43bb24f1dea46b56b6b3585a64da14ae5680f
Jakkoble <business@jakkoble.de>
2022-09-24 17:06:48 +0200

    Added Debug Interval

--- src/main/kotlin/de/jakkoble/modules/settings/Interval.kt
+++ src/main/kotlin/de/jakkoble/modules/settings/Interval.kt
@@ -1,10 +1,11 @@
 package de.jakkoble.modules.settings
 
-import de.jakkoble.utils.blockIntervall
+import de.jakkoble.utils.blockInterval
 import de.jakkoble.utils.LoreElement
 import net.kyori.adventure.text.format.NamedTextColor
 
-enum class Intervall(val displayName: String, val value: Long) {
+enum class Interval(val displayName: String, val value: Long) {
+   DEBUG("Debug", 60),
    ONE_DAY("1 Tag", 60*60*24),
    TWO_DAYS("2 Tage", 60*60*48),
    THREE_DAYS("3 Tage", 60*60*72),
@@ -13,10 +14,10 @@ enum class Intervall(val displayName: String, val value: Long) {
    SIX_DAYS("6 Tage", 60*60*144),
    SEVEN_DAYS("7 Tage", 60*60*168)
 }
-fun Intervall.format(): LoreElement {
+fun Interval.format(): LoreElement {
    var color = NamedTextColor.DARK_GRAY
    var underlined = false
-   if (this.name == blockIntervall.name) {
+   if (this.name == blockInterval.name) {
       color = NamedTextColor.DARK_GRAY
       underlined = true
    }
@@ -26,4 +27,4 @@ fun Intervall.format(): LoreElement {
       underlined = underlined
    )
 }
-fun getBlockIntervall(name: String): Intervall = Intervall.values().first { it.name == name }
+fun getBlockIntervall(name: String): Interval = Interval.values().first { it.name == name }
